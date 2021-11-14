1a37aec55809d68ffa1c7065d7867e71c465696e
k1osha <k1osha@derpy.ru>
2021-11-14 10:41:42 +1000

    db fix

--- models/FinModel.php
+++ models/FinModel.php
@@ -10,15 +10,38 @@ use Yii;
  * @property int $id
  * @property int $sort_id
  * @property string $name
- * @property string $descriprion
+ * @property string|null $descriprion
  * @property float $area
  * @property float $area_house
  * @property int $electro_price
  * @property int $water_price
  * @property string $price_culture
- * @property int $area_id
+ * @property float $exp_1
+ * @property float $prib_1
+ * @property float $price
+ * @property float $exp_2
+ * @property float $prib_2
+ * @property float $exp_3
+ * @property float $prib_3
+ * @property float $exp_4
+ * @property float $prib_4
+ * @property float $prib_5
+ * @property float $exp_5
+ * @property float $prib_6
+ * @property float $exp_6
+ * @property float $exp_7
+ * @property float $prib_7
+ * @property float $exp_8
+ * @property float $prib_8
+ * @property float $exp_9
+ * @property float $prib_9
+ * @property float $exp_10
+ * @property float $prib_10
+ * @property float $exp_11
+ * @property float $prib_11
+ * @property float $prib_12
+ * @property float $exp_12
  *
- * @property Area $area0
  * @property Expense[] $expenses
  * @property Sort $sort
  */
@@ -38,10 +61,11 @@ class FinModel extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['sort_id', 'name', 'descriprion', 'area', 'electro_price', 'water_price'], 'required'],
+            [['sort_id', 'name', 'area', 'electro_price', 'water_price', 'price_culture', 'price'], 'required'],
             [['sort_id', 'electro_price', 'water_price'], 'integer'],
-            [['area', 'area_house'], 'number'],
-            [['name', 'descriprion', 'price_culture'], 'string', 'max' => 45],
+            [['area', 'area_house', 'exp_1', 'prib_1', 'price', 'exp_2', 'prib_2', 'exp_3', 'prib_3', 'exp_4', 'prib_4', 'prib_5', 'exp_5', 'prib_6', 'exp_6', 'exp_7', 'prib_7', 'exp_8', 'prib_8', 'exp_9', 'prib_9', 'exp_10', 'prib_10', 'exp_11', 'prib_11', 'prib_12', 'exp_12'], 'number'],
+            [['name', 'price_culture'], 'string', 'max' => 45],
+            [['descriprion'], 'string', 'max' => 255],
             [['sort_id'], 'exist', 'skipOnError' => true, 'targetClass' => Sort::className(), 'targetAttribute' => ['sort_id' => 'id']],
         ];
     }
@@ -53,26 +77,40 @@ class FinModel extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'sort_id' => 'Сорт культуры',
-            'name' => 'Наименование',
-            'descriprion' => 'Описание',
+            'sort_id' => 'Сорт',
+            'name' => 'Наименование фин. модели',
+            'descriprion' => 'Описание фин. модели',
             'area' => 'Общая посевная площадь',
-            'area_house' => 'Регион',
-            'electro_price' => 'Стоимость электронергии кВт/ч',
+            'area_house' => 'Area House',
+            'electro_price' => 'Стоимость электоэнергии кВт/ч',
             'water_price' => 'Стоимость воды м3',
             'price_culture' => 'Price Culture',
-            'area_id' => 'Area ID',
+            'exp_1' => 'Exp 1',
+            'prib_1' => 'Prib 1',
+            'price' => 'Price',
+            'exp_2' => 'Exp 2',
+            'prib_2' => 'Prib 2',
+            'exp_3' => 'Exp 3',
+            'prib_3' => 'Prib 3',
+            'exp_4' => 'Exp 4',
+            'prib_4' => 'Prib 4',
+            'prib_5' => 'Prib 5',
+            'exp_5' => 'Exp 5',
+            'prib_6' => 'Prib 6',
+            'exp_6' => 'Exp 6',
+            'exp_7' => 'Exp 7',
+            'prib_7' => 'Prib 7',
+            'exp_8' => 'Exp 8',
+            'prib_8' => 'Prib 8',
+            'exp_9' => 'Exp 9',
+            'prib_9' => 'Prib 9',
+            'exp_10' => 'Exp 10',
+            'prib_10' => 'Prib 10',
+            'exp_11' => 'Exp 11',
+            'prib_11' => 'Prib 11',
+            'prib_12' => 'Prib 12',
+            'exp_12' => 'Exp 12',
         ];
-    }
-
-    /**
-     * Gets query for [[Area0]].
-     *
-     * @return \yii\db\ActiveQuery
-     */
-    public function getArea0()
-    {
-        return $this->hasOne(Area::className(), ['id' => 'area_id']);
     }
 
     /**
@@ -82,7 +120,7 @@ class FinModel extends \yii\db\ActiveRecord
      */
     public function getExpenses()
     {
-        return $this->hasMany(Expense::className(), ['fin_model_idfin_model' => 'id']);
+        return $this->hasMany(Expense::className(), ['fin_model_id' => 'id']);
     }
 
     /**
--- views/expense/_form.php
+++ views/expense/_form.php
@@ -8,67 +8,84 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="expense-form">
+<style>
+    .form-group {
+        margin: 10px;
+    }
+</style>
+
+<div class="expense-form col-10 d-flex flex-column">
 
     <?php $form = ActiveForm::begin(); ?>
+    <div class="col-6">
+        <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+        
+        <?= $form->field($model, 'price')->textInput() ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    </div>
+   
 
-    <?= $form->field($model, 'texnika_id')->textInput() ?>
+    <div class="d-flex flex-row">
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_1')->textInput() ?>
+            <?= $form->field($model, 'prib_1')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_2')->textInput() ?>
+            <?= $form->field($model, 'prib_2')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_3')->textInput() ?>
+            <?= $form->field($model, 'prib_3')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_4')->textInput() ?>
+            <?= $form->field($model, 'prib_4')->textInput() ?>
+        </div>
+    </div>
+    <br/>
+    <div class="d-flex flex-row">
+        <div class="flex-column">
+            <?= $form->field($model, 'prib_5')->textInput() ?>
+            <?= $form->field($model, 'exp_5')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'prib_6')->textInput() ?>
+            <?= $form->field($model, 'exp_6')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_7')->textInput() ?>
+            <?= $form->field($model, 'prib_7')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_8')->textInput() ?>
+            <?= $form->field($model, 'prib_8')->textInput() ?>
+        </div>
+    </div>
+    <br/>
 
-    <?= $form->field($model, 'fin_model_id')->textInput() ?>
+    <div class="d-flex flex-row">
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_9')->textInput() ?>
+            <?= $form->field($model, 'prib_9')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_10')->textInput() ?>
+            <?= $form->field($model, 'prib_10')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'exp_11')->textInput() ?>
+            <?= $form->field($model, 'prib_11')->textInput() ?>
+        </div>
+        <div class="flex-column">
+            <?= $form->field($model, 'prib_12')->textInput() ?>
+            <?= $form->field($model, 'exp_12')->textInput() ?>
+        </div>
+    </div>
+    <br/>
 
-    <?= $form->field($model, 'exp_1')->textInput() ?>
-
-    <?= $form->field($model, 'prib_1')->textInput() ?>
-
-    <?= $form->field($model, 'price')->textInput() ?>
-
-    <?= $form->field($model, 'exp_2')->textInput() ?>
-
-    <?= $form->field($model, 'prib_2')->textInput() ?>
-
-    <?= $form->field($model, 'exp_3')->textInput() ?>
-
-    <?= $form->field($model, 'prib_3')->textInput() ?>
-
-    <?= $form->field($model, 'exp_4')->textInput() ?>
-
-    <?= $form->field($model, 'prib_4')->textInput() ?>
-
-    <?= $form->field($model, 'prib_5')->textInput() ?>
-
-    <?= $form->field($model, 'exp_5')->textInput() ?>
-
-    <?= $form->field($model, 'prib_6')->textInput() ?>
-
-    <?= $form->field($model, 'exp_6')->textInput() ?>
-
-    <?= $form->field($model, 'exp_7')->textInput() ?>
-
-    <?= $form->field($model, 'prib_7')->textInput() ?>
-
-    <?= $form->field($model, 'exp_8')->textInput() ?>
-
-    <?= $form->field($model, 'prib_8')->textInput() ?>
-
-    <?= $form->field($model, 'exp_9')->textInput() ?>
-
-    <?= $form->field($model, 'prib_9')->textInput() ?>
-
-    <?= $form->field($model, 'exp_10')->textInput() ?>
-
-    <?= $form->field($model, 'prib_10')->textInput() ?>
-
-    <?= $form->field($model, 'exp_11')->textInput() ?>
-
-    <?= $form->field($model, 'prib_11')->textInput() ?>
-
-    <?= $form->field($model, 'prib_12')->textInput() ?>
-
-    <?= $form->field($model, 'exp_12')->textInput() ?>
-
-    <?= $form->field($model, 'in_stock')->textInput() ?>
+    
+    <?= $form->field($model, 'in_stock')->checkbox() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
